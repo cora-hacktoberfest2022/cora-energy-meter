0b18f7d245f5e923254bf83a436c9c7705b9d08e
Arthur Borges <arthurborges.dev@gmail.com>
2022-10-07 22:37:58 -0300

    feat: add basic skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { Logger } from "@bristom/logger";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+io.on("connection", socket => {
+  Logger.debug(`User ID ${socket.id} has connected`);
+});
+
+server.listen(3000, () => {
+  Logger.debug("listening on *:3000");
+});
