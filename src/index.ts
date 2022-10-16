ac7d370d955927e913ddf556bde5f8ed0b69c3dc
Arthur Borges <arthurborges.dev@gmail.com>
2022-10-16 19:16:11 -0300

    chore: Inital skeleton of MQTT pub/sub on same file for testing purposes, using async

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Logger } from "@bristom/logger";
+import mqtt from "async-mqtt";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
 require("dotenv").config();
 
+const client = mqtt.connect("mqtt://test.mosquitto.org");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -20,3 +22,23 @@ io.on("connection", socket => {
 server.listen(process.env.PORT, () => {
   Logger.debug(`listening on *:${process.env.PORT}`);
 });
+
+const initMqtt = async () => {
+  Logger.debug("Starting MQTT");
+  try {
+    await client.subscribe("presence");
+    await client.publish("presence", "Hello mqtt");
+  } catch (e) {
+    Logger.debug(`Something wrong with MQTT start: ${e}`);
+  }
+};
+
+client.on("connect", initMqtt);
+
+const handleMqttMessage = async (topic, message) => {
+  // message is Buffer
+  Logger.debug(message.toString());
+  await client.end();
+};
+
+client.on("message", handleMqttMessage);
