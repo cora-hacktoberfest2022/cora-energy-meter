f81d7b87fa151dfc6553a7abac716693f256d13d
Arthur Borges <arthurborges.dev@gmail.com>
2022-10-17 21:16:36 -0300

    feat: mqtt initial skeleton (#5)
    * chore: Inital skeleton of MQTT pub/sub on same file for testing purposes, using async
    
    * chore: encapsulate mqtt part in its own iot module, which will save to DB in the future
    
    * chore: remove the mqtt connection closing so the app keeps listening for the messages all the time

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
+import iot from "./mqtt";
+
 require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+iot();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
---
+++ src/mqtt.ts
@@ -0,0 +1,27 @@
+import { Logger } from "@bristom/logger";
+import mqtt from "async-mqtt";
+
+const iot = () => {
+  const client = mqtt.connect("mqtt://test.mosquitto.org");
+
+  const initMqtt = async () => {
+    Logger.debug("Starting MQTT");
+    try {
+      await client.subscribe("cora-hacktoberfest2022");
+      await client.publish("cora-hacktoberfest2022", "Hello mqtt"); // this will be inside meter/esp32
+    } catch (e) {
+      Logger.debug(`Something wrong with MQTT start: ${e}`);
+    }
+  };
+
+  client.on("connect", initMqtt);
+
+  const handleMqttMessage = async (topic, message) => {
+    // message is Buffer
+    Logger.debug(message.toString()); // save the payload to the DB in a future implementation
+  };
+
+  client.on("message", handleMqttMessage);
+};
+
+export default iot;
