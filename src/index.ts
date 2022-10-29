6027c73aada8e04b4095d2a41ef76453594f25b5
Arthur Borges <arthurborges.dev@gmail.com>
2022-10-28 21:36:02 -0300

    feat: create connection with atlas and save dummy data

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { Logger } from "@bristom/logger";
 import express from "express";
 import http from "http";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import { Server } from "socket.io";
 
 import iot from "./mqtt";
 
+const uri = "mongodb+srv://arthur:arthuradmin@coraenergymeter.yrkp3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(
+  uri,
+  { serverApi: ServerApiVersion.v1 }
+);
+
+async function dbtest() {
+  await client.connect();
+  const collection = await client.db("cora").collection("devices");
+  await collection.insertOne({ name: "test", value: 1 });
+  // perform actions on the collection object
+  client.close();
+}
+
+dbtest().catch(Logger.debug);
 require("dotenv").config();
 
 const app = express();
