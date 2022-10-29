b76e20bab0f6c5670ee8174222a4a33054008db2
Arthur Borges <arthurborges.dev@gmail.com>
2022-10-28 22:11:05 -0300

    feat: save data into DB upon POST request

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,19 @@ const client = new MongoClient(
   { serverApi: ServerApiVersion.v1 }
 );
 
+let collection;
 async function dbtest() {
   await client.connect();
-  const collection = await client.db("cora").collection("devices");
-  await collection.insertOne({ name: "test", value: 1 });
+  collection = await client.db("cora").collection("devices");
+  //await collection.insertOne({ name: "test", value: 1 });
   // perform actions on the collection object
-  client.close();
 }
 
 dbtest().catch(Logger.debug);
 require("dotenv").config();
 
 const app = express();
+app.use(express.json())
 const server = http.createServer(app);
 const io = new Server(server);
 
@@ -31,6 +32,11 @@ iot();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/", async (req, res) => {
+  await collection.insertOne({ data: req.body });
+  res.send("Data saved");
 });
 
 io.on("connection", socket => {
